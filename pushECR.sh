47839c110c67488c78c012268878e59f3a4fdbdc
Daniel Kogan <54873722+daminals@users.noreply.github.com>
2022-09-30 10:42:04 -0400

    eb

--- pushECR.sh
+++ pushECR.sh
@@ -2,3 +2,4 @@ aws ecr get-login-password --region us-east-1 | docker login --username AWS --pa
 docker build -t examplebot ./
 docker tag examplebot:latest 318710067667.dkr.ecr.us-east-1.amazonaws.com/examplebot:latest
 docker push 318710067667.dkr.ecr.us-east-1.amazonaws.com/examplebot:latest
+eb deploy
